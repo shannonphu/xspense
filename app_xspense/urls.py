6e31da58fba4354cc182eb313eaf36c45129def9
shannonphu <shannonphu@sbcglobal.net>
2015-08-02 21:39:45 -0700

    add view for all budgets

--- app_xspense/urls.py
+++ app_xspense/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	# root webpage route '/'
-	url(r'^$', views.all_expenses, name='all_expenses')
+	url(r'^expenses$', views.all_expenses, name='all_expenses'),
+	url(r'^budgets$', views.all_budgets, name='budgets')
 	#url(r'^new/budget$', views.new_budget, name='new_budget')
 ]
--- app_xspense/views.py
+++ app_xspense/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Expense
+from .models import Budget
 
 # Create your views here.
 def all_expenses(request):
 	expenses = Expense.objects.filter(date__lte=timezone.now())
 	return render(request, 'app_xspense/expenses.html', {'expenses': expenses})
+
+def all_budgets(request):
+	budgets = Budget.objects.filter(category__gte=0)
+	return render(request, 'app_xspense/budgets.html', {'budgets': budgets})

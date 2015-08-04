2c97eba5820980df247a12ffad0e928e75f78719
shannonphu <shannonphu@sbcglobal.net>
2015-08-04 12:29:57 -0700

    update register form ui

--- app_xspense/static/js/main.js
+++ app_xspense/static/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.budget:nth-child(4n)').each(function(index) {
-	    $(this).prevAll('.budget').andSelf().wrapAll('<div class="row" />');
+	    $(this).prevAll('.budget').andSelf().wrapAll('<div class="row" style="margin-bottom: 1em"/>');
 	});
 	$('#budget-expense > .budget').wrapAll('<div class="row" />');
 });

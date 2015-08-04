edcb7bbee3093ac8852cdb520e27aab7426401e8
shannonphu <shannonphu@sbcglobal.net>
2015-08-04 11:44:46 -0700

    add js dynamic adding of class row to every 4th budget div

--- app_xspense/static/js/main.js
+++ app_xspense/static/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-
+	$('.budget:nth-child(4n)').each(function(index) {
+	    $(this).prevAll('.budget').andSelf().wrapAll('<div class="row" />');
+	});
+	$('#budget-expense > .budget').wrapAll('<div class="row" />');
 });

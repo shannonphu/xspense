f5453bd2b0ee5042ea44155e6a5a5041a86e043b
shannonphu <shannonphu@sbcglobal.net>
2015-08-20 16:20:37 -0700

    more fix

--- xspense/settings.py
+++ xspense/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'xspense.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -111,7 +111,7 @@ LOGIN_URL = '/login'
 # heroku deployment
 
 # Parse database configuration from $DATABASE_URL
-# import dj_database_url
+# 
 # DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
@@ -121,9 +121,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-# import os
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),

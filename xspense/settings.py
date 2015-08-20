d1c4667bf851d8b41cf720a3babf6deda68dbd92
shannonphu <shannonphu@sbcglobal.net>
2015-08-20 16:45:23 -0700

    more trial

--- xspense/settings.py
+++ xspense/settings.py
@@ -110,22 +110,35 @@ LOGIN_URL = '/login'
 
 # heroku deployment
 
-# Parse database configuration from $DATABASE_URL
-# 
-# DATABASES['default'] =  dj_database_url.config()
+# # Parse database configuration from $DATABASE_URL
+# # 
+# # DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
+
+# # Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+DEBUG = False
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+try:
+    from .local_settings import *
+except ImportError:
+    pass
--- xspense/wsgi.py
+++ xspense/wsgi.py
@@ -16,3 +16,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xspense.settings")
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
